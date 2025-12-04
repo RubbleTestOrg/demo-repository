c415e075f15c7dc30cc34a2a860094ec85a94fc0
Matt Primrose <matt.primrose@visualvault.com>
2025-12-04 13:47:45 -0700

    feat: new test function

---
+++ Web Services/webserviceEx.js
@@ -0,0 +1,16 @@
+module.exports.main = async function (vvClient, response) {
+
+    const myName = "WebserviceEx";
+
+    function testFunction() {
+        const { a, b, ...rest } = obj;
+        console.log(a, b);
+    }
+
+    /*
+    function unusedFunction(_param1, _param2) {
+        // This function is intentionally left blank
+    }
+    */ 
+
+}
