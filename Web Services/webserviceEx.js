c5dc26b49c47c8892315df95e35bdcb25420ae18
Matt Primrose <matt.primrose@visualvault.com>
2025-12-04 13:55:45 -0700

    fix: adds jest config

--- Web Services/webserviceEx.js
+++ Web Services/webserviceEx.js
@@ -1,16 +1,14 @@
 module.exports.main = async function (vvClient, response) {
+  const myName = "WebserviceEx";
 
-    const myName = "WebserviceEx";
+  function testFunction() {
+    const { a, b, ...rest } = obj;
+    console.log(a, b);
+  }
 
-    function testFunction() {
-        const { a, b, ...rest } = obj;
-        console.log(a, b);
-    }
-
-    /*
+  /*
     function unusedFunction(_param1, _param2) {
         // This function is intentionally left blank
     }
-    */ 
-
-}
+    */
+};
